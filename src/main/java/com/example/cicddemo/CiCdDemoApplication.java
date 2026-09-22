c7a88afb68036abdb1905eaaba97809f35e52e79
teenivk <vineetkumar_dev@yahoo.com>
2026-09-22 15:43:39 +0530

    Update CiCdDemoApplication.java

--- src/main/java/com/example/cicddemo/CiCdDemoApplication.java
+++ src/main/java/com/example/cicddemo/CiCdDemoApplication.java
@@ -8,5 +8,6 @@ public class CiCdDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdDemoApplication.class, args);
+        System.out.println("Running Application");
     }
 }
